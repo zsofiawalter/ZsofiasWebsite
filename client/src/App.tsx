ed67e9b9b23f0e2d9e7c02083c30d089859028af
zsofiawalter <zsofia.walter8@gmail.com>
2024-06-30 16:33:26 +0200

    Basic front end initialized

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import pages and components
+import Home from "./pages/Home"
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
