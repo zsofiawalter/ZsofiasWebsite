c53e3e800325c4c00cee7f90fbf3ff326152b189
zsofiawalter <zsofia.walter8@gmail.com>
2024-07-09 16:15:54 +0200

    Added travels to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,18 +2,19 @@ import Logo from "./Logo";
 import NavbarItem from "./NavbarItem";
 
 function Navbar() {
-    return (
-        <header>
-            <div className="navbar">
-                <span className="left">
-                    <Logo />
-                </span>
-                <span className="right">
-                    <NavbarItem name="CV" ext="cv" />
-                </span>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="navbar">
+        <span className="left">
+          <Logo />
+        </span>
+        <span className="right">
+          <NavbarItem name="CV" ext="cv" />
+          <NavbarItem name="Travel" ext="travels" />
+        </span>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
