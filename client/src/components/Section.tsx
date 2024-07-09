e6cde02bc9925a250e32dbd976aa295d24b6d2a2
zsofiawalter <zsofia.walter8@gmail.com>
2024-07-09 11:37:08 +0200

    Move section.tsx

--- client/src/components/Section.tsx
+++ client/src/components/Section.tsx
@@ -1,13 +1,15 @@
 function Section({
   children,
   title,
+  addtlClass,
 }: {
   children: JSX.Element[];
-  title: string;
+  title?: string;
+  addtlClass?: string;
 }) {
   return (
-    <section className={title}>
-      <h3>{title}</h3>
+    <section className={title + " " + addtlClass}>
+      {title && <h3>{title}</h3>}
       {children}
     </section>
   );
